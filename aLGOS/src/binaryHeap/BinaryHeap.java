ef0a548018596cd9fb72e8b7bcf3a1eae95b2e0c
tanoaks <akshaytan2@gmail.com>
2015-10-11 02:01:54 +0530

    xzz

--- aLGOS/src/binaryHeap/BinaryHeap.java
+++ aLGOS/src/binaryHeap/BinaryHeap.java
@@ -2,8 +2,31 @@ package binaryHeap;
 
 public class BinaryHeap {
 private int array[];
-public BinaryHeap(int size) {
+private int end;
+public BinaryHeap(int size,int end) {
 	array= new int[size];
+	this.end =end;
+}
+
+public void print()
+{for(int i=0;i<array.length;i++)
+	System.out.print(array[i]+",");}
+
+public void add(int i){
+	
+	array[end]=i;
+	swim(end);
+	end++;
+}
+
+public void remove(int i)
+{
+	swap(i, end-1);
+	array[end-1]=0;
+	end--;
+	sink(i);
+	
+	
 }
 
 public void swim(int i)
@@ -26,15 +49,51 @@ public void swap(int i,int j)
 }
 public void sink(int i)
 {
-	while(i<array.length)
+	while(i<(end+1)/2) 
 	{
 		if(array[i]<array[i*2])
 		{
-			
-			
-			
+		if(array[i]<array[i*2+1])	
+		{
+			if(array[2]<array[i*2+1])
+			{
+				swap(i*2+1, i);
+			}
+			else 
+			{ 
+				swap(i*2, i);
+			}
+			i=i*2;
 		}
-		
+		else 
+		{
+			swap(i*2,i);
+			i=i*2;
+		}}
+		else 
+			if(array[i]<array[i*2+1])
+			{
+			if(array[i]<array[i*2])	
+			{
+				if(array[2]<array[i*2+1])
+				{
+					swap(i*2+1, i);
+				}
+				else 
+				{ 
+					swap(i*2, i);
+				}
+				i=i*2;
+			}
+			else 
+			{
+				swap(i*2+1,i);
+				i=i*2;
+			}
+		}
+			else
+			{break;}
+		 
 		
 	}
 }
