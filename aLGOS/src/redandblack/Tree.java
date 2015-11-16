6f01be0e64ad724927e0c4e231fd430ce5477dde
tanoaks <akshaytan2@gmail.com>
2015-11-17 01:18:08 +0530

    a

--- aLGOS/src/redandblack/Tree.java
+++ aLGOS/src/redandblack/Tree.java
@@ -1,37 +1,67 @@
 package redandblack;
 
 public class Tree {
-
+	public int value;
+	public int key;
 	Tree left;
 	Tree right;
 	 public boolean colour;
-	void flipColour()
-	{
-		if(left.colour&&right.colour)
-		{
-			this.colour=true;
-		}
+	 
+	 public Tree(int key,int value,boolean colour) {
+		this.value=value;
+		this.key=key;
+		colour=this.colour;
 	}
-	void shiftRight()
+	void flipColour(Tree h)
 	{
 		
+			h.colour=true;
+			h.left.colour=false;
+			h.right.colour=false;
+					
+	
 	}
-	void shiftLeft()
+	Tree shiftRight(Tree h)
 	{
-		if(!left.colour&&right.colour)
-		{
+		
+		Tree node;
+		node=h.left;
+		h.left=node.right;
+		node.right=h;
+		
+		return node;
+	}
+	Tree shiftLeft(Tree h)
+	{
+		
 			Tree node;
-			node=left;
-			left=left.right;
-			node.right=this;
+			node=h.right;
+		h.right=	node.left;
+			node.left=h;
 			
-			
-		}
+		return node;
 	}
 	
-	void add()
+	  Tree put(Tree h,int key ,int val)
 	{
+		 if(h==null) return new Tree(key,val,true);
+		 if(key>h.key)
+		 {
+			 put(h.right, key, val);
+		 }
+		if(key<h.key)
+		{
+			put(h.left, key, val);
+		}
+		else 
+		{
+		h.value=val;	
+		}
+		  
 		
+		
+		
+	return 	 h;
 	}
 	void get(int a)
 	{}
